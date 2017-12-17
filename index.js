41e4b8f86dff2b13c9941d605c5079c0915e6ed3
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-17 18:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 var funkyFunction = funkyFunction()
-funkyFunction();
+theFunk;
